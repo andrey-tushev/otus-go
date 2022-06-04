1e638dc68c95f859635e5768a84c39e1f4b9e53c
Andrey Tushev <avtushev@avito.ru>
2022-06-04 20:37:37 +0300

    Тесты пройдены

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,47 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	inFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	const bufSize = 1000
+	buf := make([]byte, bufSize)
+
+	if offset > 0 {
+		inFile.Seek(offset, io.SeekStart)
+	}
+
+	totalRead := 0
+	for done := false; !done; {
+		readSize, inErr := inFile.Read(buf)
+		if inErr == io.EOF {
+			return nil
+		} else if inErr != nil {
+			return inErr
+		}
+		buf = buf[:readSize]
+		totalRead += readSize
+
+		if limit > 0 && totalRead > int(limit) {
+			tail := bufSize - (totalRead - int(limit))
+			buf = buf[:tail]
+			done = true
+		}
+
+		_, outErr := outFile.Write(buf)
+		if outErr != nil {
+			return outErr
+		}
+	}
+
 	return nil
 }
