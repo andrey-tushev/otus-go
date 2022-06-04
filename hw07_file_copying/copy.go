2646b7aebe97cdcd1e9a752c0bb375424cb76925
Andrey Tushev <avtushev@avito.ru>
2022-06-04 21:08:46 +0300

    комметарии

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -12,6 +12,7 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	// Проверим размер входного файла
 	stat, err := os.Stat(fromPath)
 	if err != nil {
 		return err
@@ -23,42 +24,58 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		return ErrOffsetExceedsFileSize
 	}
 
+	// Откроем входной файл
 	inFile, err := os.Open(fromPath)
 	if err != nil {
 		return err
 	}
 	defer inFile.Close()
 
+	// Откроем выходной файл
 	outFile, err := os.Create(toPath)
 	if err != nil {
 		return err
 	}
 	defer outFile.Close()
 
-	const bufSize = 512
-	buf := make([]byte, bufSize)
-
+	// Если задано смещение, то смести позицию во входном файле
 	if offset > 0 {
 		inFile.Seek(offset, io.SeekStart)
 	}
 
+	// Буфер для чтения
+	const bufSize = 512
+	buf := make([]byte, bufSize)
+
+	// Цикл чтения по кусочкам
 	totalRead := 0
 	for done := false; !done; {
 		readSize, inErr := inFile.Read(buf)
+
+		// Входной файл закончился
 		if inErr == io.EOF {
 			return nil
-		} else if inErr != nil {
+		}
+
+		// Какая-то ошибка чтения
+		if inErr != nil {
 			return inErr
 		}
+
+		// Если прочитали меньше чем размер буфер, значит файл закончился,
+		// буфер уменьшим
 		buf = buf[:readSize]
 		totalRead += readSize
 
+		// Если задан лимит и мы его превысили, то уменьшим размер буфера,
+		// и укажем что это последняя итерация
 		if limit > 0 && totalRead > int(limit) {
 			tail := bufSize - (totalRead - int(limit))
 			buf = buf[:tail]
 			done = true
 		}
 
+		// Запишем прочитанный кусок
 		_, outErr := outFile.Write(buf)
 		if outErr != nil {
 			return outErr
