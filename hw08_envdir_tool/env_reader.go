037b0d3a98c0cfab881f169fbd7315e2ebbb781a
Andrey Tushev <avtushev@avito.ru>
2022-06-05 23:33:01 +0300

    Использованы заготовки. Сделаны тесты.

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,53 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	vars := make(Environment)
+	for _, f := range files {
+		// Если файл нулевого размера
+		ev := EnvValue{
+			NeedRemove: true,
+		}
+
+		// Если файл не нулевого размера
+		if f.Size() > 0 {
+			value, err := fetchVar(dir + "/" + f.Name())
+			if err != nil {
+				return Environment{}, err
+			}
+
+			ev = EnvValue{
+				Value:      value,
+				NeedRemove: false,
+			}
+		}
+
+		vars[f.Name()] = ev
+	}
+
+	return vars, nil
+}
+
+func fetchVar(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	value := scanner.Text()
+
+	return trim(value), nil
+}
+
+func trim(value string) string {
+	value = strings.TrimRight(value, " \t")
+	value = strings.ReplaceAll(value, "\x00", "\n")
+	return value
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
@@ -22,66 +18,11 @@ func main() {
 		args = os.Args[3:]
 	}
 
-	vars, err := getVars(dir)
+	envs, err := ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for name, value := range vars {
-		_, exists := os.LookupEnv(name)
-		if exists {
-			os.Unsetenv(name)
-		}
-		os.Setenv(name, value)
-	}
-
-	cmd := exec.Command(command, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = os.Environ()
-
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getVars(dir string) (map[string]string, error) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	vars := make(map[string]string)
-	for _, f := range files {
-		value, err := getVar(dir + "/" + f.Name())
-		if err != nil {
-			return nil, err
-		}
-
-		vars[f.Name()] = value
-	}
-
-	return vars, nil
-}
-
-func getVar(name string) (string, error) {
-	file, err := os.Open(name)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	value := scanner.Text()
-
-	return trim(value), nil
-}
-
-func trim(value string) string {
-	value = strings.TrimRight(value, " \t")
-	value = strings.ReplaceAll(value, "\x00", "\n")
-	return value
+	retCode := RunCmd(command, args, envs)
+	os.Exit(retCode)
 }
