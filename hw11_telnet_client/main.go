89350968215aa466f5c30f00bbad90b567290dea
Andrey Tushev <avtushev@avito.ru>
2022-06-19 17:57:32 +0300

    клиент с сервером научились разговаривать

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,98 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var (
+	timeout time.Duration
+	host    string
+	port    int
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Duration(10*time.Second), "connection timeout")
+}
+
 func main() {
+	// Определяем параметры вызова
+	flag.Parse()
+
+	host = flag.Arg(0)
+	if host == "" {
+		host = "localhost"
+	}
+
+	if flag.Arg(1) == "" {
+		port = 4242
+	} else {
+		port, _ = strconv.Atoi(flag.Arg(1))
+		if port <= 0 {
+			fmt.Println("bad port number")
+			os.Exit(1)
+		}
+	}
+
+	sendBuff := &bytes.Buffer{}
+
+	address := fmt.Sprintf("%s:%d", host, port)
+	net.JoinHostPort(host, strconv.Itoa(port))
+	client := NewTelnetClient(address, timeout, ioutil.NopCloser(sendBuff), os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	wg := sync.WaitGroup{}
+
+	// Читаем строчки из stdin и отправляем на сервер
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(os.Stdin)
+		for {
+			if !scanner.Scan() {
+				return
+			}
+			line := scanner.Text()
+
+			sendBuff.WriteString(line + "\n")
+			err := client.Send()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "sending connection closed")
+				return
+			}
+		}
+	}()
+
+	// Получаем строчки из сервера и печатаем их в stdout
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			err := client.Receive()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "receiving connection closed")
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Fprintf(os.Stderr, "comminication finished")
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"io"
+	"net"
 	"time"
 )
+
+const buffSize = 1024
 
 type TelnetClient interface {
 	Connect() error
@@ -12,8 +17,67 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+
+	conn net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *telnetClient) Connect() error {
+	dialer := &net.Dialer{
+		Timeout: t.timeout,
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, t.timeout)
+
+	conn, err := dialer.DialContext(ctx, "tcp", t.address)
+	if err != nil {
+		cancel()
+		return errors.New("can not establish connection")
+	}
+	t.conn = conn
+
+	return nil
+}
+
+func (t *telnetClient) Close() error {
+	return t.conn.Close()
+}
+
+func (t *telnetClient) Send() error {
+	data := make([]byte, buffSize)
+	n, _ := t.in.Read(data)
+	data = data[:n]
+
+	_, err := t.conn.Write(data)
+	return err
+}
+
+func (t *telnetClient) Receive() error {
+	data := make([]byte, buffSize)
+	n, err := t.conn.Read(data)
+	if err != nil {
+		return err
+	}
+	data = data[:n]
+
+	_, err = t.out.Write(data)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
