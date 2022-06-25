5aa14c95b4bd2fc55360417e431b11e2cdf04e65
Andrey Tushev <avtushev@avito.ru>
2022-06-25 21:07:16 +0300

    web-server

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -2,30 +2,68 @@ package internalhttp
 
 import (
 	"context"
+	"net/http"
+	"time"
 )
 
-type Server struct { // TODO
+type Server struct {
+	logger Logger
+	app    Application
 }
 
-type Logger interface { // TODO
+type Logger interface {
+	Info(msg string)
 }
 
 type Application interface { // TODO
 }
 
 func NewServer(logger Logger, app Application) *Server {
-	return &Server{}
+	return &Server{
+		logger: logger,
+		app:    app,
+	}
 }
 
 func (s *Server) Start(ctx context.Context) error {
-	// TODO
+	s.logger.Info("web-server start")
+
+	server := &http.Server{
+		Addr:         "localhost:8080",
+		Handler:      s,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	server.ListenAndServe()
+
 	<-ctx.Done()
 	return nil
 }
 
 func (s *Server) Stop(ctx context.Context) error {
-	// TODO
+	s.logger.Info("web-server stop")
+
 	return nil
 }
 
-// TODO
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.logger.Info(reqLogMessage(r))
+
+	switch r.URL.Path {
+	case "/hello":
+		w.Write([]byte("hello-world"))
+
+	default:
+		http.NotFound(w, r)
+
+	}
+}
+
+func reqLogMessage(r *http.Request) string {
+	return "http request " +
+		r.RemoteAddr + " " +
+		time.Now().String() + " " +
+		r.Method + " " +
+		r.RequestURI + " " +
+		r.Header.Get("User-Agent")
+}
