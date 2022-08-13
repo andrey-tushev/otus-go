3f1da201b5939725e49e616c848d640fe0c6eeed
Andrey Tushev <avtushev@avito.ru>
2022-08-13 20:14:56 +0300

    mutex

--- previewer/internal/cache/cache.go
+++ previewer/internal/cache/cache.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"encoding/gob"
 	"os"
+	"sync"
 
 	"github.com/andrey-tushev/otus-go/previewer/internal/preview"
 )
 
 type Cache struct {
 	dir string
+	mu  sync.RWMutex
 }
 
 func New() Cache {
@@ -19,10 +21,14 @@ func New() Cache {
 }
 
 func (c *Cache) Get(img preview.Image) *preview.Container {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	f, err := os.Open(c.filename(img))
 	if err != nil {
 		return nil
 	}
+	defer f.Close()
 
 	container := preview.NewContainer()
 	dataDecoder := gob.NewDecoder(f)
@@ -32,14 +38,20 @@ func (c *Cache) Get(img preview.Image) *preview.Container {
 }
 
 func (c *Cache) Set(img preview.Image, container *preview.Container) {
-	f, _ := os.Create(c.filename(img))
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	f, err := os.Create(c.filename(img))
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
 	var buff bytes.Buffer
 	enc := gob.NewEncoder(&buff)
 	enc.Encode(container)
 
 	_, _ = f.Write(buff.Bytes())
-	f.Close()
 }
 
 func (c *Cache) filename(img preview.Image) string {
